b09ccc9a5611aad448ec65ce04c86ee02d9d752c
Emelie <mehadhbi@hotmail.com>
2019-09-24 09:38:53 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace TODO
 {
@@ -6,7 +7,34 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            bool shouldNotExit = true;
+
+            while (shouldNotExit)
+            {
+                WriteLine("1. Add todo");
+                WriteLine("2. ");
+                WriteLine("3. Exit");
+
+                ConsoleKeyInfo pressedKey = ReadKey(true);
+
+
+                switch (pressedKey.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+
+                        break;
+
+                    case ConsoleKey.D3:
+                        shouldNotExit = false;
+                        break;
+                }
+            }
+
         }
     }
 }
